9c1c76e1a9d1fe6e96ffeb0271fc66d4ab0598eb
Chris Hodge <chrishodge87@gmail.com>
2019-07-11 16:10:34 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -6,17 +6,21 @@ length.times do |counter|
 if array[counter] == value_to_find
   return counter
 end
-
 end
-
 return nil
 
 end
 
 def find_max_value(array)
-  # Add your solution here
+
+  array.reverse!
+  return array[0] 
+ 
 end
 
 def find_min_value(array)
-  # Add your solution here
+
+array.sort!
+return array[0]
+
 end
