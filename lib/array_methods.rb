6c778b754d173909725ecd3dc8451b8d62c7f31e
Chris Hodge <chrishodge87@gmail.com>
2019-07-11 16:04:22 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -3,7 +3,9 @@ def find_element_index(array, value_to_find)
 length = array.length
 
 length.times do |counter|
-if array.counter == value_to_find return counter
+if array.counter == value_to_find 
+  return counter
+end
 
 end
 
